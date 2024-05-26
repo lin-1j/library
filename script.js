8ac551d86afd8bbc5b3929505320fff7fdb8e3be
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-05-26 22:19:07 +1000

    Allow form to display when add-book button pressed
    Also allow form to close when 'add' button in form
    is pressed

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+  
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+
+  render();
 }
+
+let addBook = document.querySelector('.add-book');
+addBook.addEventListener('click', () => {
+  let newBookForm = document.querySelector('.new-book-form');
+  newBookForm.style.display = 'flex';
+})
+
+let newBookForm = document.querySelector('.new-book-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  let newBookForm = document.querySelector('.new-book-form');
+  newBookForm.style.display = 'none';
+})
